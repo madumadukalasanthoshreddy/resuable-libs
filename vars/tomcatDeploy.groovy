697f0cc86216799206b5e3623c292a7ce978f6a5
madumadukalasanthoshreddy <106926872+madumadukalasanthoshreddy@users.noreply.github.com>
2022-07-08 22:54:44 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,11 +1,12 @@
-def call(credId,userName,ipAddrs){
+def call (credId,userName,ipAddrs) {
   sshagent(['credId']) {
  
     ipAddrs.each{ipAddr->
-        sh "mv target/*.war target/santhosh.war"
-        sh "scp -o StrictHostKeyChecking=no target/santhosh.war ${userName}@${ipAddr}:/opt/tomcat9/webapps/"
+        sh "mv target/*.war target/myapp.war"
+        sh "scp -o StrictHostKeyChecking=no target/myapp.war ${userName}@${ipAddr}:/opt/tomcat9/webapps/"
         sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/shutdown.sh"
         sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/startup.sh"
     }
   }
 }
+      
